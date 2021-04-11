80898107956d744417a21a03d305e982fba05a28
Ayşin <aysincetinn@hotmail.com>
2021-04-12 00:47:49 +0300

    12.04.21

---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Content
+    {
+        public int ContentID { get; set; }
+        public string ContentValue { get; set; }
+        public string ContentDate { get; set; }
+        //contentWriter
+        //contentTitle
+
+    }
+}
